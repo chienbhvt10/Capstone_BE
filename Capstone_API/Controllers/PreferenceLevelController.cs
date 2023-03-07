9ce4b5b0205cc6472b214ae8703c82624953729a
duongchien <chienzxzx33@gmail.com>
2023-03-08 02:44:18 +0700

    add more base structure class-interface 2

---
+++ Capstone_API/Controllers/PreferenceLevelController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Capstone_API.Controllers
+{
+    [Route("api/preference-level")]
+    [ApiController]
+    public class PreferenceLevelController : ControllerBase
+    {
+        #region Slot PreferenceLevel Api
+
+        [HttpGet("slot")]
+        public IEnumerable<string> GetSlotPreferenceLevels()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPut("slot")]
+        public void UpdateSlotPreferenceLevels()
+        {
+        }
+
+        #endregion
+
+        #region Subject PreferenceLevel Api
+
+        [HttpGet("subject")]
+        public IEnumerable<string> GetSubjectPreferenceLevels()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+
+        [HttpPut("subject")]
+        public void UpdateSubjectPreferenceLevels()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ Capstone_API/Controllers/TimeSlotController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Capstone_API.Controllers
+{
+    [Route("api/timeslot")]
+    [ApiController]
+    public class TimeSlotController : ControllerBase
+    {
+        #region TimeSlot Api
+
+        [HttpGet]
+        public IEnumerable<string> GetTimeSlots()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string GetTimeSlot()
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void CreateTimeSlot()
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateTimeSlot()
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteTimeSlot()
+        {
+        }
+
+        #endregion
+
+        #region TimeSlotCompatibility Api
+
+        [HttpGet("timeslot-compatibility")]
+        public IEnumerable<string> GetTimeSlotCompatibilities()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPut("timeslot-compatibility/{id}")]
+        public void UpdateTimeSlotCompatibility()
+        {
+        }
+
+        #endregion
+
+        #region TimeSlotConflict Api
+
+        [HttpGet("timeslot-conflict")]
+        public IEnumerable<string> GetTimeSlotConflict()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPut("timeslot-conflict/{id}")]
+        public void UpdateTimeSlotConflict()
+        {
+        }
+
+        #endregion
+    }
+}
